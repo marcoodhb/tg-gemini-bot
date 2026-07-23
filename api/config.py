3d40f2dd1081d5b1f15e9394a7fc7b7b5944a217
marcoodhb <chahuaram13@gmail.com>
2026-07-23 13:26:58 -0500

    Remove temperature setting from generation config

--- api/config.py
+++ api/config.py
@@ -62,7 +62,6 @@ send_unrecognized_content = "Contenido no reconocido enviado"
 """ read https://ai.google.dev/api/rest/v1/GenerationConfig """
 generation_config = {
     "max_output_tokens": 4096,
-    "temperature": 1.1,
 }
 
 """ read https://ai.google.dev/api/rest/v1/HarmCategory """
